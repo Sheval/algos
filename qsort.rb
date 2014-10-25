27a68d49ab3f57277c0750858739fa28333834ad
Sheval <seanator@gmail.com>
2014-10-26 00:28:58 +0300

    quick sort (oops...)

--- qsort.rb
+++ qsort.rb
@@ -4,8 +4,22 @@ require "pry"
 
 # Quick Sort implementation
 
-def partition arr, pivot
-  arr[0], arr[pivot] = arr[pivot], arr[0] if pivot > 0
+def get_pivot_index arr, type
+  case type
+  when :first
+    0
+  when :last
+    -1
+  when :median
+    { 0 => arr[0], (arr.size-1)/2 => arr[(arr.size-1)/2], -1 => arr[-1] }.sort_by(&:last)[1][0]
+  else
+    raise 'Unknown index type. Use only [:first, :median, :last]'
+  end
+end
+
+def partition arr, type
+  pivot = get_pivot_index arr, type
+  arr[0], arr[pivot] = arr[pivot], arr[0] if pivot != 0
   i = 1
   (1...arr.size).each do |j|
     if arr[j] < arr[0]
@@ -17,11 +31,12 @@ def partition arr, pivot
   return arr, i-1
 end
 
-def qsort arr
+def qsort arr, type
   @count+=arr.size-1
   return arr if arr.size <= 1
-  arr, p = partition arr, arr.size-1
-  arr = qsort(arr[0...p]) + [arr[p]] + qsort(arr[p+1..-1])
+  arr, p = partition arr, type
+  puts "Pivot: #{arr[p]}: #{arr[0...p].inspect} and #{arr[p+1..-1].inspect}"
+  arr = qsort(arr[0...p], type) + [arr[p]] + qsort(arr[p+1..-1], type)
 end
 
 
@@ -33,10 +48,19 @@ def get_from_file file_name
   arr
 end
 
-@count = 0
-p qsort [5,3,2,4,1,6]
-p @count
+sample = [3,8,2,5,1,4,7,6]
 
-@count = 0
-p qsort [3,8,2,5,1,4,7,6]
-p @count
+if $*.any?
+  sample = get_from_file($*[0])
+end
+
+
+[:first, :last, :median].each do |pivot_type|
+  puts "Array size = #{sample.size}, Pivot type: #{pivot_type}"
+  @count = 0
+  p sample
+  res = qsort sample, pivot_type
+  p res
+  puts "Comparsions count: #{@count}"
+
+end
